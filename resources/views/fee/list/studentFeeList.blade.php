ea8a23b4514bea6f6849a25fd7d1b1df393f84c2
dembee111 <minion.ck@gmail.com>
2018-02-21 19:18:17 +0800

    student fee list

---
+++ resources/views/fee/list/studentFeeList.blade.php
@@ -0,0 +1,46 @@
+<div class="panel panel-default" style="margin-top: -18px; ">
+  <div class="panel-heading">Pay list</div>
+  <div class="panel-body">
+
+    <div class="table-responsive">
+
+      <table style="border-collapse:collapse;" class="table-hover table-bordered" id="list-student-fee">
+
+        <thead>
+
+          <tr>
+                  <th style="text-align: center;">N<sup>0</sup></th>
+                  <th>Level</th>
+                  <th style="text-align: center; ">Fee</th>
+                  <th style="text-align: center; ">Amount</th>
+                  <th style="text-align: center; ">Discount</th>
+                  <th style="text-align: center; ">Paid</th>
+                  <th style="text-align: center; ">Balance</th>
+                  <th style="text-align: center; ">Action</th>
+          </tr>
+        </thead>
+
+        <tbody id="tbody-student-fee">
+
+          <tr data-id="" id="sfeeId">
+            <td style="text-align: center;"></td>
+            <td style="text-align: center;"></td>
+            <td style="text-align: center;"></td>
+            <td style="text-align: center;"></td>
+            <td style="text-align: center;"></td>
+            <td style="text-align: center;"></td>
+            <td style="text-align: center;"></td>
+            <td style="text-align: center;"></td>
+
+        </tr>
+        <tr>
+          <td colspan="9" class="hiddenRow">
+          </td>
+        </tr>
+
+      </tbody>
+    </table>
+  </div>
+</div>
+<div class="panel-footer" style="height:40px;">
+</div>
